634cd25bcb67666136566f9dc206df1d2f9ff1ed
Sangyoon Kim <eric_yoon1@naver.com>
2024-11-14 17:06:15 +0900

    [D3] Title: 평행사변형, Time: 174 ms, Memory: 22,840 KB -BaekjoonHub

---
+++ Java/SWEA/D3/15941. 평행사변형/평행사변형.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int N = sc.nextInt();
+			int area = N * N;
+			System.out.printf("#%d %d\n", test_case, area);
+		
+		}
+	}
+}
