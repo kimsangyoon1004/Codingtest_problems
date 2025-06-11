353dac13f996e01da8b5a17cc5361fd73d6a0e8f
Sangyoon Kim <eric_yoon1@naver.com>
2025-06-11 23:02:36 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.13 ms, Memory: 93.1 MB -BaekjoonHub

--- Java/프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ Java/프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,58 +1,39 @@
 import java.util.*;
+
 class Solution {
     public int solution(String dartResult) {
         int answer = 0;
         int start = 0;
         List<Integer> list = new ArrayList<>();
-        String D;
-        String S;
-        String T;
-        int d;
-        int s;
-        int t;
-        int listindex = -1;
-        for(int i = 0; i < dartResult.length(); i++){
-            if(dartResult.charAt(i) == 'D'){
-                D = dartResult.substring(start, i);
-                d = Integer.parseInt(D);
-                int Dpow = (int)Math.pow(d, 2);
-                list.add(Dpow);
-                listindex++;
+        int listIndex = -1;
+
+        for (int i = 0; i < dartResult.length(); i++) {
+            char ch = dartResult.charAt(i);
+
+            if (ch == 'S' || ch == 'D' || ch == 'T') {
+                String numStr = dartResult.substring(start, i);
+                int num = Integer.parseInt(numStr);
+                int power = (ch == 'S') ? 1 : (ch == 'D') ? 2 : 3;
+
+                list.add((int) Math.pow(num, power));
+                listIndex++;
                 start = i + 1;
-            }
-            else if(dartResult.charAt(i) == 'S'){
-                S = dartResult.substring(start, i);
-                s = Integer.parseInt(S);
-                int Spow = (int)Math.pow(s, 1);
-                list.add(Spow);
-                listindex++;
-                start = i + 1;
-            }
-            else if(dartResult.charAt(i) == 'T'){
-                T = dartResult.substring(start, i);
-                t = Integer.parseInt(T);
-                int Tpow = (int)Math.pow(t, 3);
-                list.add(Tpow);
-                listindex++;
-                start = i + 1;
-            }
-            else if(dartResult.charAt(i) == '*'){
-                if(listindex == 0){
-                    list.set(listindex, list.get(listindex) * 2);
-                }
-                else{
-                    list.set(listindex - 1, list.get(listindex - 1) * 2);
-                    list.set(listindex, list.get(listindex) * 2);
+            } else if (ch == '*') {
+                if (listIndex == 0) {
+                    list.set(listIndex, list.get(listIndex) * 2);
+                } else {
+                    list.set(listIndex - 1, list.get(listIndex - 1) * 2);
+                    list.set(listIndex, list.get(listIndex) * 2);
                 }
                 start = i + 1;
-            }
-            else if(dartResult.charAt(i) == '#'){
-                list.set(listindex, list.get(listindex) * (-1));
+            } else if (ch == '#') {
+                list.set(listIndex, list.get(listIndex) * -1);
                 start = i + 1;
             }
         }
-        for(int a : list){
-            answer += a;
+
+        for (int score : list) {
+            answer += score;
         }
         return answer;
     }
