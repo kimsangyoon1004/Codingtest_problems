4010421e4f171e21b136dcd05754f412387d2c06
Sangyoon Kim <eric_yoon1@naver.com>
2025-06-16 17:36:48 +0900

    [level 1] Title: 신고 결과 받기, Time: 202.06 ms, Memory: 169 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        Map<String, Set<String>> reportMap = new HashMap<>();
+        Map<String, Integer> countMap = new HashMap<>();
+        String[] elements = new String[2];
+        String reporter;
+        String reported;
+        for(int i = 0; i < report.length; i++){
+            elements = report[i].split(" ");
+            reporter = elements[0];
+            reported = elements[1];
+            reportMap.putIfAbsent(reporter, new HashSet<>());
+            // avoid repeating report
+            if (reportMap.get(reporter).add(reported)) {
+                countMap.put(reported, countMap.getOrDefault(reported, 0) + 1);
+            }
+        }
+        
+        for(int i = 0; i < id_list.length; i++){
+            String user = id_list[i];
+            Set<String> reportSet = reportMap.getOrDefault(user, new HashSet<>());
+            for(String target : reportSet){
+                if(countMap.get(target) >= k){
+                    answer[i]++;
+                }
+            }
+        }
+        return answer;
+    }
+}
