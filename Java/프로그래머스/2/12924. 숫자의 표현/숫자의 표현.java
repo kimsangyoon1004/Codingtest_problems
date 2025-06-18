57fbe2d48ec242a4fb412819a9207c06a22713b6
Sangyoon Kim <eric_yoon1@naver.com>
2025-06-18 18:37:36 +0900

    [level 2] Title: 숫자의 표현, Time: 0.59 ms, Memory: 53.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,27 @@
+class Solution {
+    // sliding window
+    public int solution(int n) {
+        int answer = 0;
+        int left = 1;
+        int right = 1;
+        int sum = 1;
+        while(left <= n){
+            if(sum == n){
+                answer++;
+                sum -= left;
+                left++;
+            }
+            else if(sum < n){
+                right++;
+                sum += right;
+            }
+            // sum > n
+            else{
+               sum -= left; 
+               left++;
+            }
+        }
+        
+        return answer;
+    }
+}
