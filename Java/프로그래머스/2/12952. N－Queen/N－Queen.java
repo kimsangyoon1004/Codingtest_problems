36383589ca68a410496eb372b593713c079575d6
Sangyoon Kim <eric_yoon1@naver.com>
2025-07-22 21:44:51 +0900

    [level 2] Title: N-Queen, Time: 200.62 ms, Memory: 83.3 MB -BaekjoonHub

--- Java/프로그래머스/2/12952. N－Queen/N－Queen.java
+++ Java/프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,26 +1,26 @@
 class Solution {
     public int solution(int n) {
-        return nQueen(new int[n], 0, n);
+        int answer = 0;
+        int[] queenMap = new int[n];
+        answer = nQueen(queenMap, 0, n);
+        return answer;
     }
-
-   
     static int nQueen(int[] queenMap, int row, int n){
-        if (row == n) return 1;
-
+        if(row == n){
+            return 1;
+        }
         int count = 0;
-        for (int col = 0; col < n; col++) {
-            if (isSafe(queenMap, row, col)) {
-                queenMap[row] = col;
+        for(int i = 0; i < n; i++){
+            if(isVaild(queenMap, row, i)){
+                queenMap[row] = i;
                 count += nQueen(queenMap, row + 1, n);
             }
         }
         return count;
     }
-
-    static boolean isSafe(int[] queenMap, int row, int col) {
-        for (int i = 0; i < row; i++) {
-            if (queenMap[i] == col || 
-                Math.abs(queenMap[i] - col) == Math.abs(i - row)) {
+    static boolean isVaild(int[] queenMap, int row, int col){
+        for(int i = 0; i < row; i++){
+            if(queenMap[i] == col || (Math.abs(i - row)) == (Math.abs(queenMap[i] - col))){
                 return false;
             }
         }
