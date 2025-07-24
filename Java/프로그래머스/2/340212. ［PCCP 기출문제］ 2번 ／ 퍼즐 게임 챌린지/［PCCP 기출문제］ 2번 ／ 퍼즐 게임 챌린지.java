7ac34fded53d1a9956e2f6624e44295953260f7e
Sangyoon Kim <eric_yoon1@naver.com>
2025-07-24 15:07:23 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 53.85 ms, Memory: 126 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] diffs, int[] times, long limit) {
+        int left = 1;
+        int right = 1000000;
+        int answer = 0;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (timeTook(diffs, times, mid, limit)) {
+                answer = mid;
+                right = mid - 1; 
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return answer;
+    }
+
+    static boolean timeTook(int[] diffs, int[] times, int level, long limit) {
+        long sum = 0;
+        for (int i = 0; i < diffs.length; i++) {
+            long currentTime = times[i];
+            long diff = diffs[i];
+
+            if (diff <= level) {
+                sum += currentTime;
+            } else {
+                long beforeTime = (i == 0) ? 0 : times[i - 1]; 
+                long repeat = diff - level;
+                long currentTimeTook = (currentTime + beforeTime) * repeat + currentTime;
+                sum += currentTimeTook;
+            }
+
+            if (sum > limit) return false;
+        }
+        return true;
+    }
+}
