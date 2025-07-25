ff01d629262cb60d0fb4ec04698bec3127deb3ed
Sangyoon Kim <eric_yoon1@naver.com>
2025-07-25 22:53:28 +0900

    [level 3] Title: 가장 먼 노드, Time: 78.35 ms, Memory: 105 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[][] edge) {
+        int answer = 0;
+        List<Integer> longestNode = new ArrayList<>();
+        List<HashSet<Integer>> nodes = new ArrayList<>();
+        boolean[] visited = new boolean[n + 1];
+        for(int i = 0; i <= n; i++){
+            nodes.add(new HashSet<>());
+        }
+        for(int i = 0; i < edge.length; i++){
+            nodes.get(edge[i][0]).add(edge[i][1]);
+            nodes.get(edge[i][1]).add(edge[i][0]);
+        }
+        bfs(nodes, longestNode, visited, 1);
+        int maxLength = Collections.max(longestNode);
+        int count = Collections.frequency(longestNode, maxLength);
+        return count;
+    }
+    static void bfs(List<HashSet<Integer>> nodes, List<Integer> longestNode, boolean[] visited, int start){
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{start, 0});
+        visited[start] = true;
+        
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+            int node = current[0];
+            int distance = current[1];
+            for(int n : nodes.get(node)){
+                if(!visited[n]){
+                    longestNode.add(distance + 1);
+                    queue.offer(new int[]{n, distance + 1});
+                    visited[n] = true;
+                }
+            }
+        }
+    }
+}
