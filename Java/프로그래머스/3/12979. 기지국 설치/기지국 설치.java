e3c94d60ff0c4e4f8b21f23a16e6f4e73b2d4f4a
Sangyoon Kim <eric_yoon1@naver.com>
2025-07-25 21:38:38 +0900

    [level 3] Title: 기지국 설치, Time: 1.05 ms, Memory: 55 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+        int idx = 0; 
+        int pos = 1; 
+
+        int range = 2 * w + 1;
+
+        while (pos <= n) {
+            if (idx < stations.length && pos >= stations[idx] - w) {
+                pos = stations[idx] + w + 1;
+                idx++;
+            } else {
+                answer++;
+                pos += range; 
+            }
+        }
+
+        return answer;
+    }
+}
