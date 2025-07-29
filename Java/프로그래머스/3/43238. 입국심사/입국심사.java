b2e898178f86ebece4a1601cf91000691b73b14a
Sangyoon Kim <eric_yoon1@naver.com>
2025-07-29 20:22:22 +0900

    [level 3] Title: 입국심사, Time: 89.36 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+     public static long solution(int n, int[] times) {
+        Arrays.sort(times);  
+        long left = 1;  
+        long right = (long) times[times.length - 1] * n;  
+
+        long answer = right;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long count = 0;
+            for (int time : times) {
+                count += mid / time;
+            }
+
+            if (count >= n) {
+                answer = mid;      
+                right = mid - 1;    
+            } else {
+                left = mid + 1;     
+            }
+        }
+
+        return answer;
+    }
+}
