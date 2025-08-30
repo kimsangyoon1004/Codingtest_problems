8ba0f7852d36fddc50de73f2b66bf40b2bcd9e5b
Sangyoon Kim <eric_yoon1@naver.com>
2025-08-30 23:00:19 +0900

    [level 2] Title: 올바른 괄호, Time: 24.01 ms, Memory: 55 MB -BaekjoonHub

--- Java/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ Java/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,24 +1,22 @@
 import java.util.*;
-
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (c == '(') {
-                stack.push(c);
-            } else { 
-                if (stack.isEmpty()) {
-                    return false; 
-                } else {
-                    stack.pop();
-                }
+        boolean answer = true;
+        Stack<Character> st = new Stack<Character>();
+        for(char c : s.toCharArray()){
+            if(!st.isEmpty() && c == ')' && st.peek() == '('){
+                st.pop();
+                continue;
             }
+            st.push(c);
+        }
+       if(st.isEmpty()){
+           answer = true;
+       }
+        else{
+            answer = false;
         }
 
-      
-        return stack.isEmpty();
+        return answer;
     }
 }
