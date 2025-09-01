f96eb1c0997a3fd58e18d17d257cc8af80eeaa68
Sangyoon Kim <eric_yoon1@naver.com>
2025-09-01 15:25:58 +0900

    [level 2] Title: 구명보트, Time: 10.39 ms, Memory: 57.1 MB -BaekjoonHub

--- Java/프로그래머스/2/42885. 구명보트/구명보트.java
+++ Java/프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,8 +3,7 @@ class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        int left = 0;
-        int right = people.length - 1;
+        int left = 0; int right = people.length - 1;
         while(left <= right){
             if(people[left] + people[right] > limit){
                 right--;
@@ -15,6 +14,8 @@ class Solution {
             }
             answer++;
         }
+        
+       
         return answer;
     }
 }
