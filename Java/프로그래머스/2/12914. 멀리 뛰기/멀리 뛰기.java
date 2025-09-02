9b344b1fe99315f5503ab0dbad6531ec1f24b31a
Sangyoon Kim <eric_yoon1@naver.com>
2025-09-02 17:25:57 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.19 ms, Memory: 76.6 MB -BaekjoonHub

--- Java/프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ Java/프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,17 +1,14 @@
 class Solution {
-    // bottom up
     public long solution(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-
-        long[] dp = new long[n + 1];
-        dp[1] = 1L;
-        dp[2] = 2L;
-
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        long answer = 0;
+        long[] dp = new long[n + 2];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i <= n ; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2])%1234567;
         }
-
         return dp[n];
     }
 }
